733accb6ac4b7b7c29556eccd972161d09732d3c
antrik <antrik@users.sf.net>
2008-07-20 03:25:42 +0200

    Added stubs for kgi_{,un}register_display()

--- core/core.c
+++ core/core.c
@@ -1,3 +1,18 @@
+#include <stdio.h>
+
+#include "kgi/kgi.h"
+
+kgi_s_t kgi_register_display(kgi_display_t *dpy, kgi_u_t id)
+{
+	fprintf(stderr, "kgi_register_display(dpy=%p, id=%i)\n", dpy, id);
+	return KGI_EOK;
+}
+
+void    kgi_unregister_display(kgi_display_t *dpy)
+{
+	fprintf(stderr, "kgi_unregister_display(dpy=%p)\n", dpy);
+}
+
 int main(void)
 {
 	return 0;
