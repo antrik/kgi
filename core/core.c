ed4da5e5dad0b89f089500c2ce369d89cb941886
antrik <antrik@users.sf.net>
2008-08-11 04:39:48 +0200

    Save display passed from kgim

--- core/core.c
+++ core/core.c
@@ -1,3 +1,4 @@
+#include <assert.h>
 #include <stdio.h>
 
 #include "kgi/kgi.h"
@@ -5,21 +6,27 @@
 extern int init_module(void);
 extern void cleanup_module(void);
 
+static kgi_display_t *display=NULL;
+
 kgi_s_t kgi_register_display(kgi_display_t *dpy, kgi_u_t id)
 {
 	fprintf(stderr, "kgi_register_display(dpy=%p, id=%i)\n", dpy, id);
+	display=dpy;
 	return KGI_EOK;
 }
 
 void    kgi_unregister_display(kgi_display_t *dpy)
 {
 	fprintf(stderr, "kgi_unregister_display(dpy=%p)\n", dpy);
+	display=NULL;
 }
 
 int main(void)
 {
 	init_module();
+	assert(display);
 
 	cleanup_module();
+	assert(!display);
 	return 0;
 }
